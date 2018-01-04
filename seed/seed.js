faa3affe7efb3919098799ca9bec6f887184a48d
Patryk Konior <patryk@arteia.co>
2018-01-04 17:17:02 +0100

    improved seed

--- seed/seed.js
+++ seed/seed.js
@@ -1,14 +1,16 @@
 const MongoClient = require('mongodb').MongoClient;
+const argv = require('minimist')(process.argv.slice(2));
 
 const seedData = require('./storage-items.json');
 
 const {
+    MONGODB_URI = argv.mongodb_uri,
     MONGO_HOST = 'localhost',
     MONGO_PORT = '27017',
     DB_NAME = 'iosr2017-storage'
 } = process.env;
 
-const mongoUrl = `mongodb://${MONGO_HOST}:${MONGO_PORT}/${DB_NAME}`;
+const mongoUrl = createMongoUrl();
 
 const insertItems = db => {
     console.log(`Connected to ${DB_NAME}.`);
@@ -30,3 +32,8 @@ return MongoClient.connect(mongoUrl)
     .then(insertItems)
     .then(actAfterInsert)
     .catch(actOnError);
+
+function createMongoUrl() {
+    const customUrl = `mongodb://${MONGO_HOST}:${MONGO_PORT}/${DB_NAME}`
+    return MONGODB_URI || customUrl;
+}
